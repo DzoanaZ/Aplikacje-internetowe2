e83008cd5d6de2ac4c9d78b347195efce9577287
Przeskocz <skoczp@gmail.com>
2018-05-25 12:23:46 +0200

    Szczegóły - godzina bez milisekund

--- forecast/urls.py
+++ forecast/urls.py
@@ -4,5 +4,5 @@ app_name = 'forecast'
 urlpatterns = [
    url(r'^$', views.home, name='home'),
    url(r'^update/$', views.update, name='update'),
-   url(r'^detail/(?P<div>[0-5]{1})/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<time>\d+:\d+:\d+)/$', views.detail, name='detail'),
+   url(r'^detail/(?P<div>[0-5]{1})/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<time>\d+:\d+)/$', views.detail, name='detail'),
 ]
